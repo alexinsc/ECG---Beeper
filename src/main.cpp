9d4c3af7b3e3d4829059e0e3f4a67106d2838037
Alexis Insalaco <alexis.insc@hotmail.com>
2023-11-12 09:24:00 +0100

    Code fonctionnel

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,99 @@
 #include <Arduino.h>
-#include "Tone.h"
 
-// put function declarations here:
-// Pin du buzzer
-int pinBuzzer = 9;
 
-// Pin du bouton poussoir
-int buttonPin = 6;
+// définition des Pin
+const int beeperPin = 4;
+const int pindetecteurdepoul = A0;
+float Poul;
+float BATT_S;
+const int boutonPin = 2;
+volatile bool etatProgramme = false;
 
-// Fréquences cardiaques
-int highRate = 100;
-int normalRate = 75;
-int lowRate = 50;
+void gestionnaireInterrupt() {
+  // Bascule l'état du programme lorsque l'interruption est déclenchée
+  etatProgramme = !etatProgramme;
 
-// État du beepage
-boolean beepEnabled = true;
-
-void setup() {
-  // Configuration des pins
-  pinMode(pinBuzzer, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
-
-  // Attachement de l'interruption au bouton poussoir
-  attachInterrupt(digitalPinToInterrupt(buttonPin), toggleBeep, FALLING);
-
-  // Initialisation de la communication série
-  Serial.begin(9600);
+  // Si le programme devient inactif, arrêter la tonalité
+  if (!etatProgramme) {
+    digitalWrite(beeperPin, LOW);
+  }
 }
 
-// Fonction pour basculer l'état du beepage
-void toggleBeep() {
-  beepEnabled = !beepEnabled;
+void setup() {
+  // Configure les pins
+  pinMode(beeperPin, OUTPUT);
+  pinMode(pindetecteurdepoul,INPUT);
+  pinMode(boutonPin,INPUT);
+  attachInterrupt(digitalPinToInterrupt(boutonPin),gestionnaireInterrupt, FALLING);
 }
 
 void loop() {
-  // Lire la fréquence cardiaque entrée par l'utilisateur
-  if (Serial.available() > 0) {
-    int heartbeatRate = Serial.parseInt();
-    
-    // Vérifier si le beepage est activé
-    if (beepEnabled) {
-      // Sélectionner la fréquence de beep en fonction de la fréquence cardiaque
-      if (heartbeatRate > highRate) {
-        tone(pinBuzzer, 1000); // Beep aigu
-      } else if (heartbeatRate >= lowRate) {
-        tone(pinBuzzer, 800); // Beep modéré
-      } else {
-        tone(pinBuzzer, 600); // Beep grave
-      }
-      // Pause en fonction de la fréquence cardiaque
-      delay(60000 / heartbeatRate);
-    } else {
-      noTone(pinBuzzer); // Arrêter le beepage si désactivé
-    }
+Poul=analogRead(pindetecteurdepoul);
+BATT_S  = (60/Poul)*1000 ;
+int etatBouton= digitalRead(boutonPin);
+
+
+
+if (etatProgramme){
+if (Poul>100){
+  // Génère une tonalité à 1200 Hz pendant 1 seconde
+  int frequence = 1200;
+  int duree = 1000;
+ 
+  int periodeMicros = (1000000 / frequence); // Période en microsecondes
+  int DemiPeriode = periodeMicros / 2; // Demi-période en microsecondes
+
+  for (int k = 0; k < duree * 1000; k += periodeMicros) {
+    // Cycle ON
+   
+    digitalWrite(beeperPin, HIGH);
+    delayMicroseconds(DemiPeriode);
+
+    // Cycle OFF
+    digitalWrite(beeperPin, LOW);
+    delayMicroseconds(DemiPeriode);
   }
+
+  delay(BATT_S);
+}
+else if (Poul<50){
+  // Génère une tonalité à 300 Hz pendant 1 seconde
+  int frequence = 300;
+  int duree = 1000;
+ 
+  int periodeMicros = (1000000 / frequence); // Période en microsecondes
+  int DemiPeriode = periodeMicros / 2; // Demi-période en microsecondes
+
+  for (int k = 0; k < duree  * 1000; k += periodeMicros) {
+    // Cycle ON
+    digitalWrite(beeperPin, HIGH);
+    delayMicroseconds(DemiPeriode);
+
+    // Cycle OFF
+    digitalWrite(beeperPin, LOW);
+    delayMicroseconds(DemiPeriode);
+  }
+
+  delay(BATT_S);
+}
+else  {
+  // Génère une tonalité à 600 Hz pendant 1 seconde
+  int frequence = 600;
+  int duree = 1000;
+ 
+  int periodeMicros = (1000000 / frequence); // Période en microsecondes
+  int DemiPeriode = periodeMicros / 2; // Demi-période en microsecondes
+
+  for (int k = 0; k < duree * 1000; k += periodeMicros) {
+    // Cycle ON
+   
+    digitalWrite(beeperPin, HIGH);
+    delayMicroseconds(DemiPeriode);
+
+    // Cycle OFF
+    digitalWrite(beeperPin, LOW);
+    delayMicroseconds(DemiPeriode);
+  }
+  delay(BATT_S);
+}}
 }
